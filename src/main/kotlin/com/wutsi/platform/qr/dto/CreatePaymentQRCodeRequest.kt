ffca743b5d32d17c1d19eb15420124d6df1779e9
wutsi <herve.tchepannou@gmail.com>
2021-12-30 13:56:10 -0500

    remove errors

--- src/main/kotlin/com/wutsi/platform/qr/dto/CreatePaymentQRCodeRequest.kt
+++ src/main/kotlin/com/wutsi/platform/qr/dto/CreatePaymentQRCodeRequest.kt
@@ -10,7 +10,5 @@ public data class CreatePaymentQRCodeRequest(
   public val merchantId: Long = 0,
   public val amount: Double = 0.0,
   public val currency: String = "",
-  public val description: String? = null,
-  public val invoiceId: String? = null,
   public val timeToLive: Int? = null
 )
